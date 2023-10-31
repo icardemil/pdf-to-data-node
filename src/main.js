3b6a82d67e940bc729a0542fcf7abd0e656fd5c4
Ivan Cardemil <icardemil64@gmail.com>
2023-10-30 23:20:37 -0300

    initial upload

---
+++ src/main.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.set('port', process.env.SERVER_PORT || 5000);
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method',
+	);
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
+
+// app.use('/api/ocr');
+
+app.listen(app.get('port'), () => {
+	console.log('Listen on port ', app.get('port'));
+});
