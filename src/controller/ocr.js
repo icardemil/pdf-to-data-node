609e4e51210d3d8e96ba815de2cf319cdc996b9f
Ivan Cardemil <icardemil64@gmail.com>
2023-10-31 21:53:13 -0300

    feat: convert pdf to images

--- src/controller/ocr.js
+++ src/controller/ocr.js
@@ -1,8 +1,10 @@
+const { convert } = require('pdf-img-convert');
 const multer = require('multer');
+const fs = require('fs');
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, 'tmp/pdf');
+		cb(null, 'temp/pdf');
 	},
 	filename: function (req, file, cb) {
 		cb(null, `${Date.now()}-${file.originalname}}`);
@@ -19,6 +21,35 @@ const uploadPDF = async (req, res) => {
 			0,
 			filenameNoSpaces.length - 4,
 		);
+		const outputImages = await convert(path, {
+			width: 780,
+			height: 1027,
+			page_numbers: [1, 2],
+		});
+
+		const imageFolder = `./temp/images/${filenameNormalized}`;
+		try {
+			if (!fs.existsSync(imageFolder)) {
+				fs.mkdirSync(imageFolder);
+			}
+		} catch (error) {
+			console.log(error);
+			return res.status(400).json({
+				ok: false,
+				error,
+			});
+		}
+		for (let i = 0; i < outputImages.length; i++) {
+			fs.writeFile(
+				`${imageFolder}/${filenameNormalized}-output` + i + '.png',
+				outputImages[i],
+				error => {
+					if (error) {
+						console.log('Error: ' + error);
+					}
+				},
+			);
+		}
 		return res.json({
 			ok: true,
 			filenameNormalized,
